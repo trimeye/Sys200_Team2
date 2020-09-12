302731a8a64b1025b90f2a1f3762dcac4246a7e6
TaylorJohn1234 <71104562+TaylorJohn1234@users.noreply.github.com>
2020-09-12 13:00:48 -0600

    Update login4.py

--- login4.py
+++ login4.py
@@ -7,6 +7,7 @@
 #if not, display error message to try again with 5 attempts max
 #countdown attempts remaining with increment -1
 #user reaches no attempts, login terminates
+#utilize try/except statement
 #stackOverflow to the rescue!
 
 from getpass import getpass
@@ -18,12 +19,9 @@ for i in range (5,0, -1):
     x = input("Who are you? What is your username?" )
     p = getpass()
     #print(i) 
-   try: if loginDict.get(x) and loginDict[x] == p:
-        print("access GRANTED!!!")
-        #print(i)
-        exit()
-    except: i != 1:
-        print("something is not correct, try again", i-1 , "attempts left")
+    try: loginDict.get(x) and loginDict[x] == p:print("access GRANTED!!!")
+        #print(i)exit()
+    except: i != 1:        print("something is not correct, try again", i-1 , "attempts left")
     else:
         print("too many attempts, access denied, goodbye")
 
